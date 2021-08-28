768917f2d403c9a57bb51fb9b7d0ac3b5a4813b2
hwihwi99 <chamgrace@naver.com>
2021-08-28 11:41:16 +0900

    passport 진행중

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mysql = require('mysql');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const connection = mysql.createConnection({
     host : 'localhost',
@@ -11,6 +13,7 @@ const connection = mysql.createConnection({
     database : 'NodeProject'
 })
 
+// static한 파일들은 여기서 다 가져와라!
 app.use(express.static('public'));
 
 app.listen(1000,function(){
@@ -19,6 +22,13 @@ app.listen(1000,function(){
 
 app.get('/',function(req,res){
     console.log("This is login page");
+    //res.send("<h1>Hi, Hwojeong!</h1>");
+    res.sendFile(path.join(__dirname,"../NodeJS_Project/public/welcomePage.html"));
+})
+
+app.get('/login',function(req,res){
+    console.log("This is login page");
+    //res.send("<h1>Hi, Hwojeong!</h1>");
     res.sendFile(path.join(__dirname,"../NodeJS_Project/public/welcomePage.html"));
 })
 
@@ -34,5 +44,26 @@ app.get('/LostPassword',function(req,res){
 
 app.post('/login',function(req,res){
     console.log("Login_Success");
-    res.send("<h1>This is MyPage! ReaalyWelcome!</h1>");
+    res.send("<h1>This is MyPage! ReallyWelcome!</h1>");
 })
+
+// passport 사용하기
+
+passport.serializeUser((user,done)=>{
+    console.log('passport session save : ', user.id);
+    return done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+    console.log('passport session get id : ', id);
+    done(null,id);
+})
+
+// passport.use('local-joij', new LocalStrategy({
+//     usernameField : 'email',
+//     passwordField : 'password',
+//     session : true,
+//     passReqToCallback : false
+// },function(res,email,password,done)=>{
+//     var query = connection.query('select * from user where')
+// }))
