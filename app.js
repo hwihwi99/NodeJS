12617ea105dc6bb1b452db26939685f9598aaeea
hwihwi99 <chamgrace@naver.com>
2021-08-28 17:43:00 +0900

    ejs를 이용해서 정보 보내기

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyparser = require('body-parser');
 const mysql = require('mysql');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
@@ -15,6 +16,14 @@ const connection = mysql.createConnection({
 
 // static한 파일들은 여기서 다 가져와라!
 app.use(express.static('public'));
+
+// express야 나 bodyparser를 사용할 건데, 
+// 이게 클라이언트 측에서 json 형태로 올 수도 있고 
+app.use(bodyparser.json());
+// 다른 형태로 올 수도 있어!라는 뜻 _ 인코딩 안되어 있으며 인코딩해서 보내줘!
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
 
 app.listen(1000,function(){
     console.log("1000Port Server is start")
@@ -43,8 +52,13 @@ app.get('/LostPassword',function(req,res){
 })
 
 app.post('/login',function(req,res){
-    console.log("Login_Success");
-    res.send("<h1>This is MyPage! ReallyWelcome!</h1>");
+    // 전송된 값이 req.body를 통해서 옵니다.
+    console.log(req.body.email);
+    console.log(req.body.password);
+    //res.send("<h1>This is MyPage! ReallyWelcome!"+req.body.email+"</h1>");
+    
+    // ejs를 사용하려면 render를 사용합니다!
+    res.render('email.ejs',{'email': req.body.email, 'password' : req.body.password});
 })
 
 // passport 사용하기
